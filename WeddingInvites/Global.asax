e70c8bd818c2eef5854e8a6b237fb0e1f1b9289d
unknown <d.grayson.smith@gmail.com>
2014-12-31 10:56:24 -0700

    Initial Commit

---
+++ WeddingInvites/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WeddingInvites.MvcApplication" Language="C#" %>
---
+++ WeddingInvites/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WeddingInvites.Startup))]
+namespace WeddingInvites
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
